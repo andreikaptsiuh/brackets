50167e8827db4cdb2167ba35f3477b59e8292c4c
Andrei Kaptsiuh <kaptsiuhandrei@gmail.com>
2021-01-27 00:48:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  outer: for(let i = 0; i < str.length; i++){
+    for(let j = 0; j < bracketsConfig.length; j++){
+
+      if(str[i] == bracketsConfig[j][0] && !(bracketsConfig[j][0] == bracketsConfig[j][1] && bracketsConfig[j][0] == stack.slice(-1))){
+        stack.push(bracketsConfig[j][1])
+        continue outer;
+      }
+
+      else if(str[i] == bracketsConfig[j][1]){
+
+        if(bracketsConfig[j][1] == stack.pop()){
+          continue outer;
+        }
+
+        else{
+          return false;
+        }
+
+      }
+    }
+  } 
+  return stack.length == 0;
 }
